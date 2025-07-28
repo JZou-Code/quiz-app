6157f652aa911236b0daa0d49dfaa3588fb3f254
JZou-Code <jzou2871@gmail.com>
2025-07-29 00:15:26 +1200

    Init routes.

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,21 @@
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage.jsx";
+import QuizPage from "./pages/QuizPage.jsx";
+import ResultPage from "./pages/ResultPage.jsx";
 
 function App() {
-  return (
-      <div>
-          App
-      </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<WelcomePage/>}></Route>
+                <Route path='/quiz'>
+                    <Route path='test' element={<QuizPage/>}></Route>
+                    <Route path='result' element={<ResultPage/>}></Route>
+                </Route>
+            </Routes>
+        </div>
+    )
 }
 
 export default App
