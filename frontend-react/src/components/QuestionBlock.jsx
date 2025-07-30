b644a09b97533ec2ce0d51aea0b332b45b050a49
JZou-Code <jzou2871@gmail.com>
2025-07-30 19:31:46 +1200

    Record selections in an array.

--- frontend-react/src/components/QuestionBlock.jsx
+++ frontend-react/src/components/QuestionBlock.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from '../style/QuestionBlock.module.css'
 
 const QuestionBlock = (props) => {
+    const [selected, setSelected] = useState(-1)
+
+    const returnAnswer = (idx) => {
+        setSelected(idx);
+        props.answers[props.quizNum - 1] = idx;
+    }
+
     return (
         <div className={classes.Container}>
             <div className={classes.Quiz}>
@@ -11,12 +18,18 @@ const QuestionBlock = (props) => {
             </div>
             <div className={classes.OptionContainer}>
                 {props.quizData.options.map((item, index) =>
-                    <label className={classes.Option} key={index} for={props.quizNum + '' + index}>
+                    <label
+                        className={selected === index ? `${classes.Option} ${classes.Selected}` : classes.Option}
+                        key={index}
+                        for={props.quizNum + '' + index}>
                         <input
                             type="radio"
                             name={props.quizNum}
                             id={props.quizNum + '' + index}
-                            value={index}/>
+                            value={index}
+                            checked={selected === index}
+                            onChange={() => returnAnswer(index)}
+                        />
                         <span>{String.fromCharCode(65 + index) + '. ' + item}</span>
                     </label>
                 )}
--- frontend-react/src/pages/QuizPage.jsx
+++ frontend-react/src/pages/QuizPage.jsx
@@ -4,17 +4,19 @@ import QuestionBlock from "../components/QuestionBlock.jsx";
 import quizArr from '../mock/fakeQuestions.js'
 
 const QuizPage = () => {
+    const userAnswers = quizArr.map(() => -1);
+
     return (
-            <div className={classes.Container}>
-                Quiz Page
-                {quizArr.map((item, index) =>
-                    <QuestionBlock quizData={item} quizNum={index + 1} key={index}/>
-                )}
-                <div className={classes.ButtonContainer}>
-                    <button className={classes.Submit}>Submit</button>
-                    <button className={classes.Cancel}>Cancel</button>
-                </div>
+        <div className={classes.Container}>
+            Quiz Page
+            {quizArr.map((item, index) =>
+                <QuestionBlock quizData={item} quizNum={index + 1} answers={userAnswers} key={index}/>
+            )}
+            <div className={classes.ButtonContainer}>
+                <button onClick={() => console.log(userAnswers)} className={classes.Submit}>Submit</button>
+                <button className={classes.Cancel}>Cancel</button>
             </div>
+        </div>
     );
 };
 
