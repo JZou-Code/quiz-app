3374467c8224b9af9d19f6a6ca9ab2fadf3af587
JZou-Code <jzou2871@gmail.com>
2025-08-26 19:28:48 +1200

    Update form pages to render components after every loading.

--- frontend-react/src/pages/LoginPage.jsx
+++ frontend-react/src/pages/LoginPage.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import SetupAccount from "../components/SetupAccount.jsx";
 import Header from "../components/Header.jsx";
+import PageStateContext from "../context/PageStateContext.jsx";
+import {pageState} from "../utils/pageState.js";
 
 const LoginPage = () => {
+    const ctx = useContext(PageStateContext);
+    useEffect(() => {
+        ctx.dispatch({type:pageState.LOGIN})
+    }, []);
     return (
         <div>
             <Header/>
--- frontend-react/src/pages/SignupPage.jsx
+++ frontend-react/src/pages/SignupPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import SetupAccount from "../components/SetupAccount.jsx";
 import Header from "../components/Header.jsx";
+import PageStateContext from "../context/PageStateContext.jsx";
+import {pageState} from "../utils/pageState.js";
 
 const SignupPage = () => {
-
+    const ctx = useContext(PageStateContext);
+    useEffect(() => {
+        ctx.dispatch({type:pageState.SIGNUP})
+    }, []);
     return (
         <div>
             <Header/>
