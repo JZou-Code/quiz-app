e9e985271b24e6f9db7bff0f8db2b057ae0b1d7b
JZou <jzou2871@gmail.com>
2025-09-21 16:06:25 +1200

    Update vite configuration file.

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://13.222.232.220:8000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
 
+    const proxyTarget = env.VITE_API_BASE || 'http://localhost:3000'
+
+    if (!proxyTarget) {
+        console.warn('please setup VITE_API_BASE in .env.development')
+    } else if (!/^https?:\/\//.test(proxyTarget)) {
+        console.warn('protocol required in VITE_API_BASE ，e.g: http://localhost:3000')
+    }
+
+    return {
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: proxyTarget,
+                    changeOrigin: true,
+                    rewrite: p => p.replace(/^\/api/, '')
+                }
+            }
+        }
+    }
+})
